98d02eaee6c6b89f75da371e22e73e3373825eb1
ibrahimmansur4 <ibrahimmansur4@gmail.com>
2025-02-28 16:50:10 +0500

    Refactor unit tests and add new test implementations for addition and Wi-Fi functionality

--- Simple_Unit_Test/test/test_desktop/test_native_main.cpp
+++ Simple_Unit_Test/test/test_desktop/test_native_main.cpp
@@ -1,16 +1,16 @@
 #include <unity.h>
+#include "add.h"
 
-// Function to be tested
-int add(int a, int b) {
-    return a + b;
-}
 
-// Test case 1: Test addition
 void test_addition() {
     TEST_ASSERT_EQUAL(5, add(2, 3));
     TEST_ASSERT_EQUAL(-1, add(-2, 1));
     TEST_ASSERT_EQUAL(0, add(0, 0));
 }
+
+void setUp() {}
+
+void tearDown() {}
 
 int main() {
     UNITY_BEGIN();  // Initialize Unity
